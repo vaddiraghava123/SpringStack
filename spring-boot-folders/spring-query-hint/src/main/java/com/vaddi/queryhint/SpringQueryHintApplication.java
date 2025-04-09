e41a2e767c1116f13588e3fb4b3fbea017581f74
Raghavendra Vaddi <vaddiraghava123@gmail.com>
2025-04-09 15:37:24 -0400

    Query optimization using QueryHints annoation

---
+++ spring-boot-folders/spring-query-hint/src/main/java/com/vaddi/queryhint/SpringQueryHintApplication.java
@@ -0,0 +1,13 @@
+package com.vaddi.queryhint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQueryHintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQueryHintApplication.class, args);
+	}
+
+}
---
+++ spring-boot-folders/spring-query-hint/src/test/java/com/vaddi/queryhint/SpringQueryHintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaddi.queryhint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQueryHintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
