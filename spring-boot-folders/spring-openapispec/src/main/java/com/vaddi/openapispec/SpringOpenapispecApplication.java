dc6b6f3ddb6492cbc92c5edcaefd80b7998dbcad
Raghavendra Vaddi <vaddiraghava123@gmail.com>
2026-02-11 09:04:52 -0500

    Convert from OpenAPIspec to Readme.io

---
+++ spring-boot-folders/spring-openapispec/src/main/java/com/vaddi/openapispec/SpringOpenapispecApplication.java
@@ -0,0 +1,13 @@
+package com.vaddi.openapispec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenapispecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpenapispecApplication.class, args);
+	}
+
+}
